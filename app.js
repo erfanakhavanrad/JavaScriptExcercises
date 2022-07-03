76e7cc83bc1162805299f7e8eaaec818de718912
erfanakhavanrad <erfanakhavanrad@outlook.com>
2022-07-03 12:37:37 +0430

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+// alert('hello')
+
+// console.log('logging')
+// console.log({a:5,b:9})
+// console.log(0123)
+// console.log(true)
+// console.table([1,2,3,4])
+// console.error('error')
+// console.warn('Warning')
+// console.clear()
+
+// console.time('beginning')
+// console.log('Hello World')
+// console.log('Hello World')
+// console.log('Hello World')
+// console.log('Hello World')
+// console.log('Hello World')
+// console.timeEnd('beginning')
+
+
+// var greeting = 'Hello There'
+// console.log(greeting)
+
+// let limitedScope = 'myLimitedScope'
+// console.log(limitedScope)
+
+// const name2  = 'ali'
+// console.log(name2)
+
+// name2 = 'newAli'
+
+
+// const person = {
+//     name: 'Ali',
+//     age: 34
+// }
+
+// console.log(person)
+
+// person.name = 'reza'
+// person.age = 94
+// console.log(person)
+
+
+// const numbers = [1,2,4,5,6]
+// numbers.push(222)
+// console.log(numbers);
+
+/** End of fourth session */
